5c5ee4b9d762ef567d205a41bf51e9963c21887d
JP09 <JP09@users.noreply.github.com>
2019-04-24 20:38:41 -0400

    Add files via upload

---
+++ NLP traitsPredictor/fileProcess.py
@@ -0,0 +1,65 @@
+import tweepy
+import re
+class fileProcess:
+ 	def __init__(self):
+ 		self.consumer_key = 'H8C45CY0NatTOAocN3RCcpzES'
+ 		self.consumer_secret = '5s30ceNxqy6tbHZeHycizH4oIhslTFmvhfJon5mUZVJLJDtEQd'
+ 		self.access_key = '951112705-ZxBH1BWhUsyzZi1mYelh4OPmkWcOCau4BUeomDd9'
+ 		self.access_secret = 'ukPN9spq1SJT5dexARrCUa2I1e2dnoJlvfYeqmhuCIpDC'
+ 		## selected account for testing
+ 		self.user = ["katyperry", "ConanOBrien", "Oprah", "jimmyfallon", "EmmaWatson", "LeoDiCaprio", "BillGates", "taylorswift13", "rihanna"]
+ 		self.comm = ["Padilla_Comm", "MayoClinic", "TheEconomist", "Google", "nytimes"]
+ 	def getAPI(self):
+ 		try: 
+ 			auth = tweepy.OAuthHandler(self.consumer_key, self.consumer_secret)
+ 			auth.set_access_token(self.access_key, self.access_secret)
+ 			api = tweepy.API(auth)
+ 		except:
+ 			print("Error: authentication failed")
+ 		return api
+ 	def getUserTweet(self, api):
+ 		emoji_pattern = re.compile(
+    u"(\ud83d[\ude00-\ude4f])|"  # emoticons
+    u"(\ud83c[\udf00-\uffff])|"  # symbols & pictographs (1 of 2)
+    u"(\ud83d[\u0000-\uddff])|"  # symbols & pictographs (2 of 2)
+    u"(\ud83d[\ude80-\udeff])|"  # transport & map symbols
+    u"(\ud83c[\udde0-\uddff])"  # flags (iOS)
+    "+", flags=re.UNICODE)
+ 		## user account
+ 		for each in self.user:
+ 			total = []
+ 			temp = api.user_timeline(each, count = 200)
+ 			total.extend(temp)
+ 			last = total[-1].id - 1
+ 			while len(temp) > 0:
+ 				temp = api.user_timeline(each, count = 200, max_id = last)
+ 				total.extend(temp)
+ 				last = total[-1].id - 1
+ 			result = [emoji_pattern.sub(r'', tweet.text).encode('utf-8') for tweet in total]
+ 			## write tweets to file
+ 			s = "tweet_" + each + ".txt"
+ 			with open(s, "w") as f:
+ 				for item in result:
+ 					f.write(item + "\n")
+ 			print("finish " + each + "'s tweet scraping.")
+ 		## corporate account
+ 		for each in self.comm:
+ 			total = []
+ 			temp = api.user_timeline(each, count = 200)
+ 			total.extend(temp)
+ 			last = total[-1].id - 1
+ 			while len(temp) > 0:
+ 				temp = api.user_timeline(each, count = 200, max_id = last)
+ 				total.extend(temp)
+ 				last = total[-1].id - 1
+ 			result = [emoji_pattern.sub(r'', tweet.text).encode('utf-8') for tweet in total]
+ 			## write tweets to file
+ 			s = "/home/j/NLP traitsPredictor/data/" + "tweet_" + each + ".txt"
+ 			with open(s, "w") as f:
+ 				for item in result:
+ 					f.write(item + "\n")
+ 			print("finish " + each + "'s tweet scraping.")
+## main function
+x = fileProcess()
+api = x.getAPI()
+x.getUserTweet(api)
---
+++ NLP traitsPredictor/trainBuild.py
@@ -0,0 +1,45 @@
+import re
+import string
+import pandas as pd
+from nltk.corpus import stopwords
+class trainBuild:
+	def __init__(self):
+		self.stop = list(set(stopwords.words("english")))
+		self.word  =[] ## NRC word list
+		self.better = {} ## NRC word attributes
+		self.data = pd.DataFrame() ## all data
+	## get all required values
+	def getValues(self):
+		nrc = pd.read_csv("/home/j/NLP traitsPredictor/process/better.csv", header = None, index_col = False)
+		for index, row in nrc.iterrows():
+			self.better[row[0]] = row[1:].values.tolist()
+			self.word.append(row[0])
+		self.data = pd.read_csv("//home/j/NLP traitsPredictor/mypersonality_final.csv")
+	## get attribute vectors by status
+	def getStatusProcessed(self):
+		status = [] ## processed status
+		## iterate dataframe by rows
+		for index, row in self.data.iterrows():
+			s = row["STATUS"]
+			attr = []## attribute vectors for each status
+			## status process
+			s = re.sub(r"(?:@\S*|#\S*|http(?=.*://)\S*)", "", s.rsplit("\n")[0].lower())
+			s = s.replace("rt", "").rsplit("\n")[0]
+			for word in s.translate(None, string.punctuation).split():
+				if(word in self.word and word not in self.stop):
+					attr.append(self.better[word])
+			status.append([sum(x) for x in zip(*attr)])
+		#status = filter(None, status)
+		## keep only english status, and clean the .csv file
+		label_delete = [i for i, v in enumerate(status) if not v]
+		self.data.drop(label_delete, inplace = True)
+		## update train dataset
+		self.data.to_csv("/home/j/NLP traitsPredictor/training_data/trainV1.csv", index = False, header = False)
+		mat = [] ## store processed numerical vectors
+		for index, row in self.data.iterrows():
+			mat.append(status[index] + row[2:].values.tolist())
+		## write to file
+		pd.DataFrame(mat).to_csv("//home/j/NLP traitsPredictor/training_data/trainV2.csv", index = False, header = False)
+x = trainBuild()
+x.getValues()
+x.getStatusProcessed()
